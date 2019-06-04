58f6a57a2359836153ee57d27de0062789168048
Geoff-Hignett <geoffhig@hotmail.com>
2019-06-04 18:07:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import DogList from './DogList';
+import DogDetails from './DogDetails';
+import NavBar from './NavBar';
+import whiskey from './imgs/whiskey.jpg';
+import hazel from './imgs/hazel.jpg';
+import tubby from './imgs/tubby.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: 'Whiskey',
+        age: 5,
+        src: whiskey,
+        facts: [
+          'Whiskey loves eating popcorn.',
+          'Whiskey is a terrible guard dog.',
+          'Whiskey wants to cuddle with you!'
+        ]
+      },
+      {
+        name: 'Hazel',
+        age: 3,
+        src: hazel,
+        facts: [
+          'Hazel has soooo much energy!',
+          'Hazel is highly intelligent.',
+          'Hazel loves people more than dogs.'
+        ]
+      },
+      {
+        name: 'Tubby',
+        age: 4,
+        src: tubby,
+        facts: [
+          'Tubby is not the brightest dog',
+          'Tubby does not like walks or exercise.',
+          'Tubby loves eating food.'
+        ]
+      }
+    ]
+  };
+  render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
+    return (
+      <div>
+        <NavBar dogs={this.props.dogs} />
+        <Switch>
+          <Route
+            exact
+            path="/dogs"
+            render={() => <DogList dogs={this.props.dogs} />}
+          />
+          <Route exact path="/dogs/:name" render={getDog} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
